b963f6b131a8bc1629e9c89dd853a5f5a3799580
A-lida <alida.castagnoli5@studio.unibo.it>
2024-11-28 21:15:03 +0100

    Deleted redundant expressions, added const keyword where needed, declared Boid and Predator classes final

--- include/bird.hpp
+++ include/bird.hpp
@@ -21,31 +21,31 @@ class Bird {
   virtual void set_position(Point);
   virtual void set_velocity(Point);
 
-  virtual void friction(const double, Point &);
-  virtual void boost(const double, Point &);
-  virtual Point border(const double, const double, Point);
+  virtual void friction(double, Point &);
+  virtual void boost(double, Point &);
+  virtual Point border(double, double, Point);
   // virtual void rotateTriangle(sf::VertexArray &, double) ;
 
-  virtual Point separation(const double, const double, std::vector<std::shared_ptr<Bird>> &) = 0;
+  virtual Point separation(double, double, std::vector<std::shared_ptr<Bird>> &) = 0;
 
   virtual ~Bird();
 };
 
-class Boid : public Bird {
+class Boid final : public Bird {
  public:
   Boid();
   Boid(Point const &, Point const &);  // dentro chiama Bird()
 
-  Point separation(const double, const double, std::vector<std::shared_ptr<Bird>> &) override;
-  Point alignment(const double, std::vector<std::shared_ptr<Bird>> &);
-  Point cohesion(const double, std::vector<std::shared_ptr<Bird>> &);
+  Point separation(double, double, std::vector<std::shared_ptr<Bird>> &) override;
+  Point alignment(double, std::vector<std::shared_ptr<Bird>> &);
+  Point cohesion(double, std::vector<std::shared_ptr<Bird>> &);
 };
 
-class Predator : public Bird {
+class Predator final : public Bird {
  public:
   Predator();
   Predator(Point const &, Point const &);
-  Point separation(const double, const double, std::vector<std::shared_ptr<Bird>> &) override;
+  Point separation(double, double, std::vector<std::shared_ptr<Bird>> &) override;
 };
 
 #endif
--- src/bird.cpp
+++ src/bird.cpp
@@ -1,8 +1,5 @@
 #include "../include/bird.hpp"
 
-#include <SFML/Graphics/Vertex.hpp>
-#include <SFML/Graphics/VertexArray.hpp>
-#include <cmath>
 #include <memory>
 #include <numeric>
 #include <vector>
@@ -12,13 +9,13 @@
 
 // Implementation of Bird class
 Bird::Bird() = default;
-Bird::Bird(Point const& position, Point const& velocity) : position_(position), velocity_(velocity){};
+Bird::Bird(Point const& position, Point const& velocity) : position_(position), velocity_(velocity) {};
 
 Point Bird::get_position() const { return position_; }
 Point Bird::get_velocity() const { return velocity_; }
 
-void Bird::set_position(Point position) { position_ = position; };
-void Bird::set_velocity(Point velocity) { velocity_ = velocity; };
+void Bird::set_position(const Point position) { position_ = position; };
+void Bird::set_velocity(const Point velocity) { velocity_ = velocity; };
 
 void Bird::friction(const double maxSpeed, Point& velocity) {
   if (velocity.module() > maxSpeed) {
@@ -46,35 +43,34 @@ Point Bird::border(const double margin, const double turn_factor, Point v) {
   if (position_.get_y() > (graphic_par::windowHeight - margin)) {
     v4_y -= turn_factor;
   }
-  return Point(v4_x, v4_y);
+  return {v4_x, v4_y};
 };
 
-Bird::~Bird(){};
+Bird::~Bird() = default;
 
 // Implementation of Boid class
 // non mi è chiaro perchè io non possa assegnare i valori nella lista
-Boid::Boid() : Bird(){};
-Boid::Boid(Point const& pos, Point const& vel) : Bird(pos, vel){};
+Boid::Boid() : Bird() {};
+Boid::Boid(Point const& pos, Point const& vel) : Bird(pos, vel) {};
 
 Point Boid::separation(const double s, const double ds, std::vector<std::shared_ptr<Bird>>& near) {
-  Point v1{0., 0.};
+  // Point v1{0., 0.};
+  // Point x1 = near[i]->get_position();
 
-  for (int i = 0; i < static_cast<int>(near.size()); ++i) {
-    Point x1 = near[i]->get_position();
-
-    // We are supposing that the near vector does not contain the current boid,
-    // for this reason we implemented the flight rules with near.size() instead of near.size()-1
-
-    if (x1.distance(position_) < ds) {
-      v1 += -s * (x1 - position_);
-    }
-  }
-  return v1;  // todo METTERE FOR_EACH
+  const Point sum = std::accumulate(near.begin(), near.end(), Point(0., 0.),
+                                    [this, ds](Point acc, const std::shared_ptr<Bird>& boid) {
+                                      if (boid->get_position().distance(position_) < ds) {
+                                        acc += boid->get_position() - position_;
+                                      }
+                                      return acc;
+                                    });
+  return (-s) * sum;
 };
 
 Point Boid::alignment(const double a, std::vector<std::shared_ptr<Bird>>& near) {
-  Point sum = std::accumulate(near.begin(), near.end(), Point(0., 0.),
-                              [](Point acc, std::shared_ptr<Bird> boid) { return acc + boid->get_velocity(); });
+  const Point sum =
+      std::accumulate(near.begin(), near.end(), Point(0., 0.),
+                      [](const Point acc, const std::shared_ptr<Bird>& boid) { return acc + boid->get_velocity(); });
 
   // TODO mettere assert: near_.size() > 1, e che 0 < a < 1
 
@@ -84,10 +80,9 @@ Point Boid::alignment(const double a, std::vector<std::shared_ptr<Bird>>& near) 
 };
 
 Point Boid::cohesion(const double c, std::vector<std::shared_ptr<Bird>>& near) {
-  // Point sum{0., 0.};
-
-  Point sum = std::accumulate(near.begin(), near.end(), Point(0., 0.),
-                              [](Point acc, std::shared_ptr<Bird> boid) { return acc + boid->get_position(); });
+  const Point sum =
+      std::accumulate(near.begin(), near.end(), Point(0., 0.),
+                      [](const Point acc, const std::shared_ptr<Bird>& boid) { return acc + boid->get_position(); });
 
   // Subtracting from the nearest boids' center of mass the current boid's
   // position and then multiplying for a cohesion factor
@@ -97,28 +92,16 @@ Point Boid::cohesion(const double c, std::vector<std::shared_ptr<Bird>>& near) {
 };
 
 // Implementation of Predator class
-Predator::Predator() : Bird(){};
-Predator::Predator(Point const& pos, Point const& vel) : Bird(pos, vel){};
+Predator::Predator() : Bird() {};
+Predator::Predator(Point const& pos, Point const& vel) : Bird(pos, vel) {};
+
 Point Predator::separation(const double s, const double ds, std::vector<std::shared_ptr<Bird>>& near) {
-  // Point v1{0., 0.};
-
-  Point v1 =
-      std::accumulate(near.begin(), near.end(), Point(0., 0.), [this, s, ds](Point acc, std::shared_ptr<Bird> boid) {
-        if (boid->get_position().distance(position_) < ds) {
-          acc += -s * 0.2 * (boid->get_position() - position_);
-        }
-        return acc;
-      });
-
-  // for (int i = 0; i < static_cast<int>(near.size()); ++i) {
-  //   Point x1 = near[i]->get_position();
-
-  //   // We are supposing that the near vector does not contain the current boid,
-  //   // for this reason we implemented the flight rules with near.size() instead of near.size()-1
-
-  //   if (x1.distance(position_) < ds) {
-  //     v1 += -s * (x1 - position_);
-  //   }
-  // }
-  return v1;
+  const Point sum = std::accumulate(near.begin(), near.end(), Point(0., 0.),
+                                    [this, ds](Point acc, const std::shared_ptr<Bird>& boid) {
+                                      if (boid->get_position().distance(position_) < ds) {
+                                        acc += boid->get_position() - position_;
+                                      }
+                                      return acc;
+                                    });
+  return (-s * 0.02) * sum;
 };
