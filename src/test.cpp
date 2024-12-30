03129cbfd0feece9f500c08c2adbd702257f3954
A-lida <alida.castagnoli5@studio.unibo.it>
2024-12-30 12:51:57 +0100

    fixed Boid's tests, added test for predators

--- src/test.cpp
+++ src/test.cpp
@@ -36,7 +36,7 @@ TEST_CASE("Testing Point class") {
     CHECK((p1.get_y() == 1.));
   }
   SUBCASE("Testing module method") {
-    CHECK((p0.module() == 0));
+    CHECK((p0.module() == 0.));
     CHECK((p1.module() == doctest::Approx(1.41421356)));
     CHECK((p2.module() == doctest::Approx(6.41112315)));
   }
@@ -53,9 +53,9 @@ TEST_CASE("Testing Point class") {
 
   SUBCASE("Testing angle method") {
     CHECK((p1.angle() == doctest::Approx(3./4 * M_PI)));
-    CHECK((p2.angle() == doctest::Approx(3./2 * M_PI - 0.9460137803)));
-    CHECK((p3.angle() == doctest::Approx(2 * M_PI - 1.1071487178)));
-    CHECK((p4.angle() == doctest::Approx(M_PI/2. - 1.4056476494)));
+    CHECK((p2.angle() == doctest::Approx(3./2 * M_PI - 0.62478254650161)));
+    CHECK((p3.angle() == doctest::Approx(-0.463647609)));
+    CHECK((p4.angle() == doctest::Approx(0.165148675)));
   }
 
   SUBCASE("Testing operator +") {
@@ -190,14 +190,14 @@ TEST_CASE("Testing Point class") {
     v1 = p1();
     v2 = p2();
 
-    CHECK((v0.position.x == doctest::Approx(p0.get_x())));
-    CHECK((v0.position.y == doctest::Approx(p0.get_y())));
+    CHECK((v0.position.x == doctest::Approx(0.)));
+    CHECK((v0.position.y == doctest::Approx(0.)));
 
-    CHECK((v1.position.x == doctest::Approx(p1.get_x())));
-    CHECK((v1.position.y == doctest::Approx(p1.get_y())));
+    CHECK((v1.position.x == doctest::Approx(1.)));
+    CHECK((v1.position.y == doctest::Approx(1.)));
 
-    CHECK((v2.position.x == doctest::Approx(p2.get_x())));
-    CHECK((v2.position.y == doctest::Approx(p2.get_y())));
+    CHECK((v2.position.x == doctest::Approx(-5.2)));
+    CHECK((v2.position.y == doctest::Approx(3.75)));
   }
 };
 
@@ -210,16 +210,16 @@ TEST_CASE("Testing Boid class") {
   double x = pos1.get_x();
   double y = pos1.get_y();
 
-  Point pos2 = pos1 + 0.8 * d * Point(std::cos( M_PI/3. + vel1.angle()), std::sin( M_PI/3. + vel1.angle()));
+  Point pos2 = pos1 + 0.8 * d * Point(std::sin( M_PI/3. + vel1.angle()), -std::cos( M_PI/3. + vel1.angle()));
   Point vel2(-8.5, -6.);
 
-  Point pos3 = pos1 + 0.8 * d * Point(std::cos(-1 * M_PI/3. + vel1.angle()), std::sin( -1 * M_PI/3. + vel1.angle()));
+  Point pos3 = pos1 + 0.8 * d * Point(std::sin(-1 * M_PI/3. + vel1.angle()), -std::cos( -1 * M_PI/3. + vel1.angle()));
   Point vel3(-7.3, 2.5);
 
-  Point pos4 = pos1 + 0.5 * ds * Point(std::cos(M_PI/3. + vel1.angle()), std::sin(M_PI/3. + vel1.angle()));
+  Point pos4 = pos1 + 0.5 * ds * Point(std::sin(M_PI/3. + vel1.angle()), -std::cos(M_PI/3. + vel1.angle()));
   Point vel4(-1., -2.);
 
-  Point pos5 = pos1 + 0.5 * ds * Point(std::cos(-1 * M_PI/3. + vel1.angle()), std::sin( -1 * M_PI/3. + vel1.angle()));
+  Point pos5 = pos1 + 0.5 * ds * Point(std::sin(-1 * M_PI/3. + vel1.angle()), -std::cos( -1 * M_PI/3. + vel1.angle()));
   Point vel5(-5., -3.2);
 
   Boid b0;
@@ -227,12 +227,14 @@ TEST_CASE("Testing Boid class") {
   Boid b2(pos2, vel2);
   Boid b3(pos3, vel3);
   Boid b4(pos4, vel4);
-  Boid b5(pos5, vel5);
+  Boid b5;
+  b5.set_bird(pos5, vel5);
+//  b5.set_velocity(vel5);
 
   std::vector<std::shared_ptr<Bird>> near_b1 {std::make_shared<Boid>(b2), std::make_shared<Boid>(b3),
       std::make_shared<Boid>(b4), std::make_shared<Boid>(b5)};
 
-  SUBCASE("Testing getters") {
+  SUBCASE("Testing getters and setters") {
     CHECK((b0.get_position().get_x() == 0.));
     CHECK((b0.get_position().get_y() == 0.));
     CHECK((b0.get_velocity().get_x() == 0.));
@@ -245,16 +247,16 @@ TEST_CASE("Testing Boid class") {
   }
 
   SUBCASE("Testing separation method") {
-    double sep1_x = -s * 0.5 * ds * (std::cos(M_PI/3. + vel1.angle()) + std::cos(-1 * M_PI/3. + vel1.angle()));
-    double sep1_y = -s * 0.5 * ds * (std::sin(M_PI/3. + vel1.angle()) + std::sin(-1 * M_PI/3. + vel1.angle()));
+    double sep1_x = -s * 0.5 * ds * (std::sin(M_PI/3. + vel1.angle()) + std::sin(-1 * M_PI/3. + vel1.angle()));
+    double sep1_y = -s * 0.5 * ds * -(std::cos(M_PI/3. + vel1.angle()) + std::cos(-1 * M_PI/3. + vel1.angle()));
 
     CHECK((b1.separation(s, ds, near_b1).get_x() == doctest::Approx(sep1_x)));
     CHECK((b1.separation(s, ds, near_b1).get_y() == doctest::Approx(sep1_y)));
   }
 
   SUBCASE("Testing cohesion method") {
-    double coh1_x = c * ((4* x +( 0.5 * ds + 0.8 * d)*( std::cos(M_PI/3. + vel1.angle()) + std::cos(-1 * M_PI/3. + vel1.angle())) )/4. - x);
-    double coh1_y = c * ((4* y +( 0.5 * ds + 0.8 * d)*( std::sin(M_PI/3. + vel1.angle()) + std::sin(-1 * M_PI/3. + vel1.angle())) )/4. - y);
+    double coh1_x = c * ((4* x +( 0.5 * ds + 0.8 * d) * (std::sin(M_PI/3. + vel1.angle()) + std::sin(-1 * M_PI/3. + vel1.angle())) )/4. - x);
+    double coh1_y = c * ((4* y +( 0.5 * ds + 0.8 * d) * -(std::cos(M_PI/3. + vel1.angle()) + std::cos(-1 * M_PI/3. + vel1.angle())) )/4. - y);
 
     CHECK((b1.cohesion(c, near_b1).get_x() == doctest::Approx(coh1_x)));
     CHECK((b1.cohesion(c, near_b1).get_y() == doctest::Approx(coh1_y)));
@@ -262,10 +264,10 @@ TEST_CASE("Testing Boid class") {
 
   SUBCASE("Testing alignment method") {
      double al1_x = a * ((b2.get_velocity().get_x() + b3.get_velocity().get_x() + b4.get_velocity().get_x() + b5.get_velocity().get_x()) / 4. -
-                 b1.get_velocity().get_x());
+                 5.);
 
      double al1_y = a * ((b2.get_velocity().get_y() + b3.get_velocity().get_y() + b4.get_velocity().get_y() +  b5.get_velocity().get_y()) / 4. -
-                 b1.get_velocity().get_y());
+                 -1.);
 
     CHECK((b1.alignment(a, near_b1).get_x() == doctest::Approx(al1_x)));
     CHECK((b1.alignment(a, near_b1).get_y() == doctest::Approx(al1_y)));
@@ -274,11 +276,11 @@ TEST_CASE("Testing Boid class") {
   SUBCASE("Testing repel method") {
     //anche se la funzione è pensata per essere usata sui predatori, usiamo il vettore di boid vicini
 
-    double rep1_x = -s * 6 * (0.5 * ds + 0.8 * d ) * (std::cos(M_PI/3. + vel1.angle()) + std::cos(-1 * M_PI/3. + vel1.angle()));
-    double rep1_y = -s * 6 * (0.5 * ds + 0.8 * d ) * (std::sin(M_PI/3. + vel1.angle()) + std::sin(-1 * M_PI/3. + vel1.angle()));
+    double rep1_x = -s * 6 * (0.5 * ds + 0.8 * d ) * (std::sin(M_PI/3. + vel1.angle()) + std::sin(-1 * M_PI/3. + vel1.angle()));
+    double rep1_y = -s * 6 * (0.5 * ds + 0.8 * d ) * -(std::cos(M_PI/3. + vel1.angle()) + std::cos(-1 * M_PI/3. + vel1.angle()));
 
     CHECK((b1.repel(s, near_b1).get_x() == doctest::Approx(rep1_x)));
-    CHECK((b1.alignment(s, near_b1).get_y() == doctest::Approx(rep1_y)));
+    CHECK((b1.repel(s, near_b1).get_y() == doctest::Approx(rep1_y)));
   }
 
     Point v1 = vel1;
@@ -294,48 +296,72 @@ TEST_CASE("Testing Boid class") {
     b4.friction(maxSpeed, v4);
     b5.friction(maxSpeed, v5);
 
-    CHECK((v1.get_x() == doctest::Approx(maxSpeed[0] * std::cos(vel1.angle()))));
-    CHECK((v1.get_y() == doctest::Approx(maxSpeed[0] * std::sin(vel1.angle()))));
+    CHECK((v1.get_x() == doctest::Approx(maxSpeed[0] * std::sin(vel1.angle()))));
+    CHECK((v1.get_y() == doctest::Approx(-maxSpeed[0] * std::cos(vel1.angle()))));
 
-    CHECK((v2.get_x() == doctest::Approx(maxSpeed[0] * std::cos(vel2.angle()))));
-    CHECK((v2.get_y() == doctest::Approx(maxSpeed[0] * std::sin(vel2.angle()))));
+    CHECK((v2.get_x() == doctest::Approx(maxSpeed[0] * std::sin(vel2.angle()))));
+    CHECK((v2.get_y() == doctest::Approx(-maxSpeed[0] * std::cos(vel2.angle()))));
 
-    CHECK((v3.get_x() == doctest::Approx(maxSpeed[0] * std::cos(vel3.angle()))));
-    CHECK((v3.get_y() == doctest::Approx(maxSpeed[0] * std::sin(vel3.angle()))));
+    CHECK((v3.get_x() == doctest::Approx(maxSpeed[0] * std::sin(vel3.angle()))));
+    CHECK((v3.get_y() == doctest::Approx(-maxSpeed[0] * std::cos(vel3.angle()))));
 
     CHECK((v4.get_x() == doctest::Approx(vel4.get_x())));
     CHECK((v4.get_y() == doctest::Approx(vel4.get_y())));
 
-    CHECK((v5.get_x() == doctest::Approx(maxSpeed[0] * std::cos(vel5.angle()))));
-    CHECK((v5.get_y() == doctest::Approx(maxSpeed[0] * std::sin(vel5.angle()))));
+    CHECK((v5.get_x() == doctest::Approx(maxSpeed[0] * std::sin(vel5.angle()))));
+    CHECK((v5.get_y() == doctest::Approx(-maxSpeed[0] * std::cos(vel5.angle()))));
   }
 
   SUBCASE("Testing boost method"){
-    b1.boost(maxSpeed, v1);
-    b2.boost(maxSpeed, v2);
-    b3.boost(maxSpeed, v3);
-    b4.boost(maxSpeed, v4);
-    b5.boost(maxSpeed, v5);
+    b1.boost(minSpeed, v1);
+    b2.boost(minSpeed, v2);
+    b3.boost(minSpeed, v3);
+    b4.boost(minSpeed, v4);
+    b5.boost(minSpeed, v5);
 
-    CHECK((v1.get_x() == doctest::Approx(vel1.get_x())));
-    CHECK((v1.get_y() == doctest::Approx(vel1.get_y())));
+    CHECK((v1.get_x() == doctest::Approx(5.)));
+    CHECK((v1.get_y() == doctest::Approx(-1.)));
 
-    CHECK((v2.get_x() == doctest::Approx(vel2.get_x())));
-    CHECK((v2.get_y() == doctest::Approx(vel2.get_y())));
+    CHECK((v2.get_x() == doctest::Approx(-8.5)));
+    CHECK((v2.get_y() == doctest::Approx(-6.)));
 
-    CHECK((v3.get_x() == doctest::Approx(vel3.get_x())));
-    CHECK((v3.get_y() == doctest::Approx(vel3.get_y())));
+    CHECK((v3.get_x() == doctest::Approx(-7.3)));
+    CHECK((v3.get_y() == doctest::Approx(2.5)));
 
-    CHECK((v4.get_x() == doctest::Approx(minSpeed[0] * std::cos(vel4.angle()))));
-    CHECK((v4.get_y() == doctest::Approx(minSpeed[0] * std::sin(vel4.angle()))));
+    CHECK((v4.get_x() == doctest::Approx(minSpeed[0] * std::sin(vel4.angle()))));
+    CHECK((v4.get_y() == doctest::Approx(-minSpeed[0] * std::cos(vel4.angle()))));
 
-    CHECK((v5.get_x() == doctest::Approx(vel5.get_x())));
-    CHECK((v5.get_y() == doctest::Approx(vel5.get_y())));
+    CHECK((v5.get_x() == doctest::Approx(-5.)));
+    CHECK((v5.get_y() == doctest::Approx(-3.2)));
   }
-
+  //TODO: implementare test per border()
  };
-//
-//  TEST_CASE("Testing Predator class") {
-//    SUBCASE(""){}
-//
-//  };
+
+TEST_CASE("Testing Predator class") {
+      Point pos1 (3., -2.);
+      Point vel1 (5., -1.);
+      Predator p1 (pos1, vel1);
+      Predator p2;
+      Predator p3;
+
+      Point pos2 = pos1 + 0.8 * d * Point(std::sin( M_PI/3. + vel1.angle()), -std::cos( M_PI/3. + vel1.angle()));
+      Point vel2(-8.5, -6.);
+      p2.set_bird(pos2, vel2);
+
+      Point pos3 = pos1 + 0.8 * d * Point(std::sin(-1 * M_PI/3. + vel1.angle()), -std::cos( -1 * M_PI/3. + vel1.angle()));
+      Point vel3(-7.3, 2.5);
+      p3.set_bird(pos3, vel3);
+
+      std::vector<std::shared_ptr<Bird>> near_p1 {std::make_shared<Predator>(p2), std::make_shared<Predator>(p3)};
+
+      SUBCASE("Testing chase method"){
+        //nel test applichiamo la funzione chase su near_p1, i predatori vicini a p1, anche se sarebbe pensata per near_boids
+        double chase1_x = c * 2 * ((0.8 * d * (std::sin(M_PI/3. + vel1.angle()) + std::sin(-1 * M_PI/3. + vel1.angle())) /2.));
+        double chase1_y = c * 2 * ((0.8 * d * -(std::cos(M_PI/3. + vel1.angle()) + std::cos(-1 * M_PI/3. + vel1.angle())) /2.));
+
+        CHECK((p1.chase(c, near_p1).get_x() == doctest::Approx(chase1_x)));
+        CHECK((p1.chase(c, near_p1).get_y() == doctest::Approx(chase1_y)));
+
+      }
+
+};
