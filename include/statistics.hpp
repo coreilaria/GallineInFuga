f51bef4cb660f3581fbf6109bdbe2c269a7b36f0
A-lida <alida.castagnoli5@studio.unibo.it>
2025-01-02 02:19:38 +0100

    minor changes

--- include/statistics.hpp
+++ include/statistics.hpp
@@ -1,18 +1,14 @@
 #ifndef STATISTICS_HPP
 #define STATISTICS_HPP
 
-struct Statistics{
-    double mean_dist;
-    double dev_dist;
-    double mean_speed;
-    double dev_speed;
-    
-    Statistics();
-    Statistics(double, double, double, double);
+struct Statistics {
+  double mean_dist;
+  double dev_dist;
+  double mean_speed;
+  double dev_speed;
 
-
+  Statistics();
+  Statistics(double, double, double, double);
 };
-
-
 
 #endif
--- src/statistics.cpp
+++ src/statistics.cpp
@@ -1,5 +1,5 @@
 #include "../include/statistics.hpp"
 
-
-Statistics::Statistics(): mean_dist{0.},  dev_dist{0.}, mean_speed{0.}, dev_speed{0.} {};
-Statistics::Statistics(const double m_dist, const double d_dist, const double m_speed, const double d_speed): mean_dist{m_dist},  dev_dist{d_dist}, mean_speed{m_speed}, dev_speed{d_speed} {};
+Statistics::Statistics() : mean_dist{0.}, dev_dist{0.}, mean_speed{0.}, dev_speed{0.} {}
+Statistics::Statistics(const double m_dist, const double d_dist, const double m_speed, const double d_speed)
+    : mean_dist{m_dist}, dev_dist{d_dist}, mean_speed{m_speed}, dev_speed{d_speed} {}
