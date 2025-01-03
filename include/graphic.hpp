6296566a65a3bae52b1a018cc02c71a004dd9f34
A-lida <alida.castagnoli5@studio.unibo.it>
2025-01-03 19:56:01 +0100

    Added description for graphic_par and triangles namespaces. Changed the attributes' name

--- include/graphic.hpp
+++ include/graphic.hpp
@@ -12,18 +12,40 @@
 //----------------------------------------------------------------------------------------------------------------------
 namespace graphic_par {
 
+///@param dt It represents the temporal interval used to update cinematic quantities.
 constexpr double dt = 0.7;
 
+///@param windowWidth It represents the width of the simulation window.
 constexpr float windowWidth = 1900.f;
+
+///@param windowHeight It represents the height of the simulation window.
 constexpr float windowHeight = 900.f;
+
+///@param statsWidth It represents the width of the window containing the statistics data.
 constexpr float statsWidth = 0.25 * windowWidth;
 
+///@param maxVel_x It represents the upper boundary of the random generation range for the component x of the velocity
+/// of a bird.
 constexpr double maxVel_x = 5.;
+
+///@param minVel_x It represents the lower boundary of the random generation range for the component x of the velocity
+/// of a bird.
 constexpr double minVel_x = -maxVel_x;
 
+///@param maxVel_y It represents the upper boundary of the random generation range for the component y of the velocity
+/// of a bird.
 constexpr double maxVel_y = 3;
+
+///@param minVel_y It represents the lower boundary of the random generation range for the component y of the velocity
+/// of a bird.
 constexpr double minVel_y = -maxVel_y;
 
+///@brief It takes in input an integer, checking if it should be strictly positive or just positive. It allows 3
+/// attempts to insert a valid input, then it exit the program.
+///@param prompt Is a constant string that will be streamed in output.
+///@param positive Is a boolean constant that determine if the output of the function should be strictly positive or
+/// just positive.
+///@return An integer.
 int getPositiveInteger(const std::string& prompt, bool positive);
 }  // namespace graphic_par
 
@@ -32,19 +54,41 @@ int getPositiveInteger(const std::string& prompt, bool positive);
 //----------------------------------------------------------------------------------------------------------------------
 namespace triangles {
 
-constexpr float baseWidth_ = 7;
-constexpr float height_ = 15;
-const sf::Vector2f relative_position[6] = {sf::Vector2f(0, -height_ / 2),
-                                           sf::Vector2f(-baseWidth_ / 2, height_ / 2),
-                                           sf::Vector2f(baseWidth_ / 2, height_ / 2),
-                                           sf::Vector2f(0, (-height_ * 3 / 2) / 2),
-                                           sf::Vector2f((-baseWidth_ * 3 / 2) / 2, (height_ * 3 / 2) / 2),
-                                           sf::Vector2f((baseWidth_ * 3 / 2) / 2, (height_ * 3 / 2) / 2)};
+///@param baseWidth_ Is the length of the triangle's base.
+constexpr float baseWidth = 7;
 
-void createTriangles(const flock::Flock&, sf::VertexArray&);
-void rotateTriangle(const std::shared_ptr<Bird>&, sf::VertexArray&, double, int);
-float getBaseWidth();
-float getHeight();
+///@param height Is the height of the triangle.
+constexpr float height = 15;
+
+///@param relativePosition Is an array containing the relative position of a triangle's vertex in relation to the
+/// center. The first three elements identify a triangle associated with a Boid object, the last three identify a
+/// triangle associated with a Predator object.
+const sf::Vector2f relativePosition[6] = {sf::Vector2f(0, -height / 2),
+                                          sf::Vector2f(-baseWidth / 2, height / 2),
+                                          sf::Vector2f(baseWidth / 2, height / 2),
+                                          sf::Vector2f(0, (-height * 3 / 2) / 2),
+                                          sf::Vector2f((-baseWidth * 3 / 2) / 2, (height * 3 / 2) / 2),
+                                          sf::Vector2f((baseWidth * 3 / 2) / 2, (height * 3 / 2) / 2)};
+
+/// @brief Construct an array of sf::Vertex, three to three represent triangles, one for each bird.
+/// @param flock Is the vector of birds.
+/// @param triangles Is an array containing tree sf::Vertex for each bird in the flock, those constitute a sf::Triangle.
+void createTriangles(const flock::Flock& flock, sf::VertexArray& triangles);
+
+/// @brief Update the direction of the triangle.
+/// @details Rotate the triangle associated with the bird, according to the direction of the bird's velocity.
+/// @param bird Is the bird associated with the triangle we need to rotate.
+/// @param triangles Is an array containing tree sf::Vertex for each bird in the flock, those constitute a sf::Triangle.
+/// @param theta Is the angle of the bird's updated velocity, formed with the vertical axis.
+void rotateTriangle(const std::shared_ptr<Bird>& bird, sf::VertexArray& triangles, double theta, int i);
+
+/// @brief Get the base width of the triangle.
+/// @return The base with.
+[[nodiscard]] float getBaseWidth();
+
+/// @brief Get the height of the triangle.
+/// @return The height with.
+[[nodiscard]] float getHeight();
 }  // namespace triangles
 
 #endif
--- src/graphic.cpp
+++ src/graphic.cpp
@@ -42,56 +42,56 @@ void createTriangles(const flock::Flock& flock, sf::VertexArray& triangles) {
 
     if (i < flock.getBoidsNum()) {
       // upper vertex (centered)
-      triangles[j].position = vertex.position + sf::Vector2f(0, -height_ / 2);
+      triangles[j].position = vertex.position + sf::Vector2f(0, -height / 2);
       triangles[j].color = sf::Color::Blue;
 
       // down-left vertex
-      triangles[j + 1].position = vertex.position + sf::Vector2f(-baseWidth_ / 2, height_ / 2);
+      triangles[j + 1].position = vertex.position + sf::Vector2f(-baseWidth / 2, height / 2);
       triangles[j + 1].color = sf::Color::Blue;
 
       // down-right vertex
-      triangles[j + 2].position = vertex.position + sf::Vector2f(baseWidth_ / 2, height_ / 2);
+      triangles[j + 2].position = vertex.position + sf::Vector2f(baseWidth / 2, height / 2);
       triangles[j + 2].color = sf::Color::Blue;
 
     } else {
       // upper vertex (centered)
-      triangles[j].position = vertex.position + sf::Vector2f(0, -(height_ * 3 / 2) / 2);
+      triangles[j].position = vertex.position + sf::Vector2f(0, -(height * 3 / 2) / 2);
       triangles[j].color = sf::Color::Red;
 
       // down-left vertex
-      triangles[j + 1].position = vertex.position + sf::Vector2f(-(baseWidth_ * 3 / 2) / 2, (height_ * 3 / 2) / 2);
+      triangles[j + 1].position = vertex.position + sf::Vector2f(-(baseWidth * 3 / 2) / 2, (height * 3 / 2) / 2);
       triangles[j + 1].color = sf::Color::Red;
 
       // down-right vertex
-      triangles[j + 2].position = vertex.position + sf::Vector2f((baseWidth_ * 3 / 2) / 2, (height_ * 3 / 2) / 2);
+      triangles[j + 2].position = vertex.position + sf::Vector2f((baseWidth * 3 / 2) / 2, (height * 3 / 2) / 2);
       triangles[j + 2].color = sf::Color::Red;
     }
   }
   assert(static_cast<int>(triangles.getVertexCount()) == flock.getFlockSize() * 3);
 }
 
-void rotateTriangle(const std::shared_ptr<Bird>& bird, sf::VertexArray& triangle, const double theta, const int i) {
+void rotateTriangle(const std::shared_ptr<Bird>& bird, sf::VertexArray& triangles, const double theta, const int i) {
   const sf::Vertex vertex{bird->getPosition()()};  // operator () returns conversion from Point to sf::Vertex
 
   const int j = 3 * i;
 
   for (int k = j; k < j + 3; ++k) {
     if (std::dynamic_pointer_cast<Boid>(bird)) {
-      triangle[k].position =
-          vertex.position + sf::Vector2f(static_cast<float>(relative_position[k - j].x * std::cos(theta) -
-                                                            relative_position[k - j].y * std::sin(theta)),
-                                         static_cast<float>(relative_position[k - j].x * std::sin(theta) +
-                                                            relative_position[k - j].y * std::cos(theta)));
+      triangles[k].position =
+          vertex.position + sf::Vector2f(static_cast<float>(relativePosition[k - j].x * std::cos(theta) -
+                                                            relativePosition[k - j].y * std::sin(theta)),
+                                         static_cast<float>(relativePosition[k - j].x * std::sin(theta) +
+                                                            relativePosition[k - j].y * std::cos(theta)));
     } else if (std::dynamic_pointer_cast<Predator>(bird)) {
-      triangle[k].position =
-          vertex.position + sf::Vector2f(static_cast<float>(relative_position[k - j + 3].x * std::cos(theta) -
-                                                            relative_position[k - j + 3].y * std::sin(theta)),
-                                         static_cast<float>(relative_position[k - j + 3].x * std::sin(theta) +
-                                                            relative_position[k - j + 3].y * std::cos(theta)));
+      triangles[k].position =
+          vertex.position + sf::Vector2f(static_cast<float>(relativePosition[k - j + 3].x * std::cos(theta) -
+                                                            relativePosition[k - j + 3].y * std::sin(theta)),
+                                         static_cast<float>(relativePosition[k - j + 3].x * std::sin(theta) +
+                                                            relativePosition[k - j + 3].y * std::cos(theta)));
     }
   }
 }
 
-float getBaseWidth() { return baseWidth_; }
-float getHeight() { return height_; }
+float getBaseWidth() { return baseWidth; }
+float getHeight() { return height; }
 }  // namespace triangles
